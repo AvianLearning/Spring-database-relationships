29cb90239a54ae82fa8788841355c4c796db1ca9
AvianLearning <iaradcliffe@hotmail.com>
2020-12-01 10:28:35 +0000

    Added Employee class

--- src/main/java/com/codeclan/example/company_relationships/models/Employee.java
+++ src/main/java/com/codeclan/example/company_relationships/models/Employee.java
@@ -1,11 +1,30 @@
 package com.codeclan.example.company_relationships.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "employee_number")
     private int employeeNumber;
     //be able to be assigned to multiple projects
-
+    @ManyToOne
+    @JoinColumn(name = "department_id", nullable = false)
+    @JsonIgnoreProperties({"employees"})
+    private Department department;
 
     public Employee(String firstName, String lastName, int employeeNumber) {
         this.firstName = firstName;
@@ -39,5 +58,13 @@ public class Employee {
 
     public void setEmployeeNumber(int employeeNumber) {
         this.employeeNumber = employeeNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
